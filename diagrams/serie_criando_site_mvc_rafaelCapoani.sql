99226cd0e24ad9cee45b3294eefb0ce33d1c92c7
Levirroh <johann.gr14@gmail.com>
2025-01-14 18:32:09 -0300

    adding the diagrams created for the mvc site

---
+++ diagrams/serie_criando_site_mvc_rafaelCapoani.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE serie_criando_site_mvc;
+USE serie_criando_site_mvc;
+
+CREATE TABLE postagem(
+	id_postagem INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    titulo_postagem VARCHAR(200) NOT NULL,
+    conteudo_postagem TEXT NOT NULL
+);
+
+CREATE TABLE comentario(
+	id_comentario INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    autor_comentario VARCHAR (200) NOT NULL,
+    mensagem_comentario TEXT NOT NULL,
+    fk_postagem INT NOT NULL,
+    FOREIGN KEY (fk_postagem) REFERENCES postagem(id_postagem)
+); 
