f4c2f3a7d0e9637d39f3dc03297345de8cb8f94c
Levirroh <johann.gr14@gmail.com>
2025-01-22 10:06:30 -0300

    criando sistema de deleção de comentarios e de postagens.

--- App/Controller/AdminController.php
+++ App/Controller/AdminController.php
@@ -35,9 +35,49 @@ Class AdminController{
             echo '<script>alert("Publicação inserida com sucesso!");</script>';
             echo '<script>location.href="http://localhost/learning_mvc/?pagina=admin&metodo=index"</script>';
         } catch(Exception $e){
-            echo '<script>alert("asdasd"");</script>';
+            echo '<script>alert("Erro ao inserir publicação");</script>';
             echo '<script>location.href="http://localhost/learning_mvc/?pagina=admin&metodo=create"</script>';
         }
+    }
+    public function change($paramId){
+        $loader = new \Twig\Loader\FilesystemLoader('App/View');
+        $twig = new \Twig\Environment($loader); 
+        $template = $twig->load('update.html'); 
+
+        $post = Postagem::selecionaPorId($paramId);
+
+        $parametros = array();
+        $parametros['id'] = $post->id_postagem;
+        $parametros['titulo'] = $post->titulo_postagem;
+        $parametros['conteudo'] = $post->conteudo_postagem;
+
+        $conteudo = $template->render($parametros);
+        echo $conteudo;
+    }
+    public function update()
+    {
+        try{
+            Postagem::update($_POST);
+            echo '<script>alert("Publicação alterada com sucesso!");</script>';
+            echo '<script>location.href="http://localhost/learning_mvc/?pagina=admin&metodo=index"</script>';
+        } catch(Exception $e){
+            echo '<script>alert("Erro ao alterar a publicação");</script>';
+            echo '<script>location.href="http://localhost/learning_mvc/?pagina=admin&metodo=change&id='.$_POST['id'].'"</script>';
+        }
+
+    }
+
+    public function delete($paramId)
+    {
+        try{
+             Postagem::delete($paramId);
+            echo '<script>alert("Publicação deletada com sucesso!");</script>';
+            echo '<script>location.href="http://localhost/learning_mvc/?pagina=admin&metodo=index"</script>';
+        } catch(Exception $e){
+            echo '<script>alert("Erro ao deletar a publicação");</script>';
+            echo '<script>location.href="http://localhost/learning_mvc/?pagina=admin&metodo=index"</script>';
+        }
+       
     }
 
 }
--- App/Model/Postagem.php
+++ App/Model/Postagem.php
@@ -53,6 +53,49 @@ class Postagem{
         return true;
 
     }
+
+
+    public static function update($params)
+    {
+        $con = Connection::getConn();
+
+        $sql = "UPDATE postagem SET titulo_postagem = :tit, conteudo_postagem = :cont WHERE id_postagem = :id";
+        $sql = $con->prepare($sql);
+        $sql->bindValue(':tit', $params['titulo']);
+        $sql->bindValue(':cont', $params['conteudo']);
+        $sql->bindValue(':id', $params['id']);
+        $resultado = $sql->execute();
+
+        if ($resultado == 0){
+            throw new Exception("Falha ao atualizar postagem.");
+            return false;
+        }
+
+        return true;
+
+
+    }
+
+    public static function delete($id){
+        $con = Connection::getConn();
+
+        $sql_comentario = "DELETE FROM comentario WHERE fk_postagem = :id";
+        $sql_comentario = $con->prepare($sql_comentario);
+        $sql_comentario->bindValue(':id', $id);
+        $resultado_comentario = $sql_comentario->execute();
+        
+        $sql_postagem = "DELETE FROM postagem WHERE id_postagem = :id";
+        $sql_postagem = $con->prepare($sql_postagem);
+        $sql_postagem->bindValue(':id', $id);
+        $resultado_postagem = $sql_postagem->execute();
+        
+        if ($resultado_postagem == 0 OR $sql_comentario == 0){
+            throw new Exception("Falha ao deletar postagem.");
+            return false;
+        }
+
+        return true;
+    }
     
 
 }
